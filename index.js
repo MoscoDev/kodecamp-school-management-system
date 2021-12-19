bdc07048991aed87a43ee49530d64f5acfaddb16
Audu Moses <89123661+MoscoDev@users.noreply.github.com>
2021-12-19 21:44:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 const auth = require("./middleware/auth")
 const authRole = require("./middleware/adminAuth")
 const {getAllTeachers} = require('./modules/admin/handler')
-const cors = require('cors')
+// const cors = require('cors')
 // handles public assets
 app.use(express.static('public'));
-app.use(cors());
+// app.use(cors());
 app.use(express.json({urlEncoded: false}))
 // ejs template engine -> for parsing and using javascript in the html files
 app.set('view engine', 'ejs');
