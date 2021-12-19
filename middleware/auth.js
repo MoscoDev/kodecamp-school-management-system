080daa01f5b847137823fd3ecc436c34e000dc24
Audu Moses <89123661+MoscoDev@users.noreply.github.com>
2021-12-19 21:41:44 +0100

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+// require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const { SECRET } = process.env;
 const { tokenModel} = require("../modules/admin/model");
